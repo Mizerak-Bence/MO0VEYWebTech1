42b72c7a61cf79c33b67660dc724cee1ac764e30
mo0vey <artixbence@gmail.com>
2025-11-19 13:23:51 +0100

    KÉÉÉSZ KÉÉÉSZ#

--- MO0VEY_1119/MO0VEY_2.js
+++ MO0VEY_1119/MO0VEY_2.js
@@ -1,29 +1,36 @@
 $(document).ready(function(){
+    var jqText = $("#jqLink").text();
+
     $("#k1").on("click", function(e){
         e.preventDefault();
-        $("#lista1 li:lt(2), #jqLink").hide();
+        $("#lista1 li:lt(2), #jqLink").toggle();
     });
 
     $("#k2").on("click", function(e){
         e.preventDefault();
-        $("#lista1 li:lt(2), #jqLink").hide();
-        $(this).hide();
+        $("#lista1 li:lt(2), #jqLink").toggle();
+        $(this).toggle();
     });
 
     $("#k3").on("click", function(e){
         e.preventDefault();
-        $("#fejlec, #lista1 li:lt(2), #jqLink").hide();
+        $("#fejlec, #lista1 li:lt(2), #jqLink").toggle();
     });
 
     $("#k4").on("click", function(e){
         e.preventDefault();
-        $("#lista1 li:lt(2), #jqLink").hide();
-        $("#jqLink").text("");
+        if ($("#lista1 li:first").is(":visible")) {
+            $("#lista1 li:lt(2), #jqLink").hide();
+            $("#jqLink").text("");
+        } else {
+            $("#lista1 li:lt(2), #jqLink").show();
+            $("#jqLink").text(jqText);
+        }
     });
 
     $("#k5").on("click", function(e){
         e.preventDefault();
-        $("#lista1 li:lt(2), #jqLink").hide();
-        $("#tabla1 tr:even").hide();
+        $("#lista1 li:lt(2), #jqLink").toggle();
+        $("#tabla1 tr:even").toggle();
     });
 });
